01eb1ec10cdb11bcd4056e9d539931f0eb06fadc
awtw <alec.wang.tpe@gmail.com>
2019-12-27 00:05:05 +0800

    before sleep, ch09-109 Hierarchical injector

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+	selector: 'app-new-account',
+	templateUrl: './new-account.component.html',
+	styleUrls: [ './new-account.component.css' ],
+	providers: [ LoggingService, AccountsService ]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+	@Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
-  onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
-  }
+	constructor(private loggingService: LoggingService, private accountsService: AccountsService) {}
+
+	onCreateAccount(accountName: string, accountStatus: string) {
+		this.accountsService.addAccount(accountName, accountStatus);
+		this.loggingService.logStatusChange(accountStatus);
+	}
 }
