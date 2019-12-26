7c457a4adb80a428357fa4c31c3ffdd46034654b
awtw <alec.wang.tpe@gmail.com>
2019-12-27 07:03:19 +0800

    ch09-111 before work

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -5,8 +5,8 @@ import { AccountsService } from '../accounts.service';
 @Component({
 	selector: 'app-account',
 	templateUrl: './account.component.html',
-	styleUrls: [ './account.component.css' ],
-	providers: [ LoggingService, AccountsService ]
+	styleUrls: [ './account.component.css' ]
+	// providers: [ LoggingService ]
 })
 export class AccountComponent {
 	@Input() account: { name: string; status: string };
@@ -18,7 +18,7 @@ export class AccountComponent {
 	onSetTo(status: string) {
 		// this.statusChanged.emit({ id: this.id, newStatus: status });
 		this.accountsService.updateStatus(this.id, status);
-		this.loggingService.logStatusChange(status);
-		console.log('A server status changed, new status: ' + status);
+		// this.loggingService.logStatusChange(status);
+		// console.log('A server status changed, new status: ' + status);
 	}
 }
